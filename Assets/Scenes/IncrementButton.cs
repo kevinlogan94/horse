0c197e2c503742bdd89dab62d3c968cd4cd45213
Kevin Logan <klogan@navistone.com>
2020-07-30 22:52:34 -0400

    add +1 animation

--- Assets/Scenes/IncrementButton.cs
+++ Assets/Scenes/IncrementButton.cs
@@ -1,17 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using TMPro;
 using UnityEngine;
 
 public class IncrementButton : MonoBehaviour
 {
+    private ObjectPooler _objectPooler;
+    private AudioManager _audioManager;
+
+    public void Start()
+    {
+        _objectPooler = ObjectPooler.Instance;
+        _audioManager = FindObjectOfType<AudioManager>();
+    }
+    
     public void Increment(int increment = 1)
     {
         var randomNumber = Random.Range(0.0f, 3.0f);
-        FindObjectOfType<AudioManager>().Play("Cork", randomNumber);
+        _audioManager.Play("Cork", randomNumber);
+        var obj = _objectPooler.SpawnFromPool("IncrementText");
         if (randomNumber<0.5)
         {
             increment*=3;
         }
+        obj.GetComponent<TextMeshProUGUI>().text = "+" + increment;
         Monitor.IncrementHorses(increment);
+        
+        //https://forum.unity.com/threads/hide-object-after-time.291287/
+        StartCoroutine(RemoveAfterSeconds(1, obj));
+        Debug.Log("end");
     }
+
+    private static IEnumerator RemoveAfterSeconds(int seconds, GameObject obj)
+    {
+        yield return new WaitForSeconds(seconds);
+        obj.SetActive(false);
+    }
+
 }
