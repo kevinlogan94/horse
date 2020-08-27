f2289cea6ff07e8f48f725ca1d66008704063a4e
Kevin Logan <klogan@navistone.com>
2020-08-26 22:01:49 -0400

    left over files

---
+++ Assets/Scripts/Achievements/ProgressBar.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+using Slider = UnityEngine.UI.Slider;
+
+public class ProgressBar : MonoBehaviour
+{
+    public void UpdateText()
+    {
+        var slider = gameObject.GetComponent<Slider>();
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().text = slider.value + "/" + slider.maxValue;
+    }
+}
