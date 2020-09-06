a434addd25d4f677bed32b5a7ce0a556cc43cd45
Kevin Logan <klogan@navistone.com>
2020-09-06 18:42:01 -0400

    more changes added

---
+++ Assets/Scripts/AnimationEvents/Lock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    public GameObject HorseUIPanel;
+    
+    public void DisableActiveState()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ActivateNewHorsePanel()
+    {
+        HorseUIPanel.SetActive(true);
+    }
+}
