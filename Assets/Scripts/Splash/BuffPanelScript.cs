1c3b121d4bb63375912efb2adb7caf034ae5706b
Kevin Logan <klogan@navistone.com>
2020-10-25 21:37:05 -0400

    add buff part 2

---
+++ Assets/Scripts/Splash/BuffPanelScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BuffPanelScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CollectAndCloseSplash()
+    {
+        BuffManager.Instance.TriggerBuff(BuffType.Mana, 15);
+        SplashManager.Instance.CloseSplash();
+    }
+
+    public void WatchAdAndCloseSplash()
+    {
+        AdvertisementManager.Instance.ShowBuffRewardAd(BuffType.Mana, 30);
+        SplashManager.Instance.CloseSplash();
+    }
+}
